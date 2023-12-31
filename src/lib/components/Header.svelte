17a71b5094c45c30db0982264bb98ee3c288f83f
Kyle Nakamura <knakamura13dev@gmail.com>
2023-12-31 15:55:20 -0800

    Replace sveltekit template with custom design and header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { browser } from '$app/environment';
+
+	export let title = 'SvelteKit';
+
+	if (browser && document.title) title = document.title;
+</script>
+
+<header>
+	<a href="/" class="header-title">{title}</a>
+</header>
+
+<style lang="scss">
+	header {
+		display: flex;
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		z-index: 9999;
+		background-color: var(--back-2);
+		height: var(--nav-height);
+		justify-content: center;
+		align-items: center;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		.header-title {
+			text-align: center;
+			margin: auto;
+			color: var(--text-2);
+		}
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,15 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'$components/*': './src/lib/components/*',
+			'$assets/*': './src/lib/images/*',
+			'$routes/*': './src/routes/*'
+		}
 	}
 };
 
