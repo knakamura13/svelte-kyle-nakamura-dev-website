8b02f40213df31560046df5c61b8f6d0afbab07e
Kyle Nakamura <knakamura13dev@gmail.com>
2024-01-07 16:36:56 -0800

    Add a simple back button to Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import AnimatedButton from '$lib/components/AnimatedButton.svelte';
+	import { page } from '$app/stores';
+
+	$: urlPath = $page.url.pathname;
 </script>
 
 <AppBar shadow="shadow-2xl">
 	<svelte:fragment slot="lead">
+		{#if urlPath !== '/'}
+			<button
+				class="btn back-btn"
+				title="Back"
+				on:click={() => {
+					history.back();
+				}}
+			>
+				<img src="/icons/icon-arrow-light.svg" alt="" class="rotate-180 cursor-pointer" />
+			</button>
+		{/if}
+
 		<a href="/" class="link link--zoomies">
 			<strong class="text-xl uppercase">Kyle Nakamura</strong>
 		</a>
@@ -25,3 +40,15 @@
 		>
 	</svelte:fragment>
 </AppBar>
+
+<style lang="scss">
+	.btn.back-btn {
+		padding: 0;
+		height: 100%;
+		margin-right: 0.5rem;
+
+		img {
+			height: 2.2rem;
+		}
+	}
+</style>
